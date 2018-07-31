02d7a4633f24682a685bc97fcffbfb2ad0b2d4f1
javier <javier@javier-PC>
2018-07-31 00:04:46 -0400

    Cambien la visual de login

--- ProyectoFinal2/src/Visual/Login.java
+++ ProyectoFinal2/src/Visual/Login.java
@@ -77,63 +77,40 @@ public class Login extends JDialog {
 		contentPane.add(panel, BorderLayout.CENTER);
 		panel.setLayout(null);
 		
-		JPanel panel_1 = new JPanel();
-		panel_1.setBackground(new Color(0, 0, 102));
-		panel_1.setBounds(0, 0, 299, 547);
-		panel.add(panel_1);
-		panel_1.setLayout(null);
-		
-		JPanel panel_3 = new JPanel();
-		panel_3.setBackground(new Color(0, 0, 102));
-		panel_3.setBounds(57, 105, 183, 229);
-		panel_1.add(panel_3);
-		panel_3.setLayout(null);
-		
-		JLabel lblNewLabel = new JLabel("");
-		lblNewLabel.setIcon(new ImageIcon(Login.class.getResource("/imagenes/hombre-de-negocios (2).png")));
-		lblNewLabel.setBounds(28, 16, 168, 197);
-		panel_3.add(lblNewLabel);
-		
-		JLabel lblBancoPopular = new JLabel("Bolsa de Empleados 1.0");
-		lblBancoPopular.setFont(new Font("Tahoma", Font.BOLD, 17));
-		lblBancoPopular.setForeground(new Color(255, 255, 255));
-		lblBancoPopular.setBounds(39, 477, 225, 20);
-		panel_1.add(lblBancoPopular);
-		
 		JPanel panel_2 = new JPanel();
 		panel_2.setBackground(Color.WHITE);
-		panel_2.setBounds(297, 0, 345, 562);
+		panel_2.setBounds(0, 0, 642, 562);
 		panel.add(panel_2);
 		panel_2.setLayout(null);
 		
-		JLabel label = new JLabel("Iniciar Seccion");
-		label.setForeground(new Color(0, 0, 102));
-		label.setFont(new Font("Tahoma", Font.BOLD, 35));
-		label.setBounds(46, 44, 293, 99);
-		panel_2.add(label);
+		JLabel lblIniciarSeccion = new JLabel("Iniciar Seccion:");
+		lblIniciarSeccion.setForeground(Color.RED);
+		lblIniciarSeccion.setFont(new Font("Arial", Font.BOLD | Font.ITALIC, 35));
+		lblIniciarSeccion.setBounds(50, 23, 293, 99);
+		panel_2.add(lblIniciarSeccion);
 		
 		JLabel label_1 = new JLabel("Usuario:");
 		label_1.setForeground(new Color(0, 0, 102));
 		label_1.setFont(new Font("Tahoma", Font.BOLD, 18));
-		label_1.setBounds(143, 182, 92, 20);
+		label_1.setBounds(46, 309, 92, 20);
 		panel_2.add(label_1);
 		
 		txtUsuario = new JTextField();
 		txtUsuario.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		txtUsuario.setColumns(10);
-		txtUsuario.setBounds(46, 235, 271, 55);
+		txtUsuario.setBounds(265, 291, 271, 55);
 		panel_2.add(txtUsuario);
 		
 		JLabel label_2 = new JLabel("Contrasena:");
 		label_2.setForeground(new Color(0, 0, 102));
 		label_2.setFont(new Font("Tahoma", Font.BOLD, 18));
-		label_2.setBounds(120, 334, 114, 20);
+		label_2.setBounds(46, 417, 114, 20);
 		panel_2.add(label_2);
 		
 		txtPassword = new JPasswordField();
 		txtPassword.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		txtPassword.setColumns(10);
-		txtPassword.setBounds(46, 387, 271, 55);
+		txtPassword.setBounds(265, 389, 271, 55);
 		panel_2.add(txtPassword);
 		
 		JButton button = new JButton("Iniciar Seccion");
@@ -145,7 +122,7 @@ public class Login extends JDialog {
 				String clavedef=new String(clave);
 				
 				
-				if (txtUsuario.getText().equals("Administrador")&& clavedef.equals("12345")) {	
+				if (txtUsuario.getText().equals("Alonso") && clavedef.equals("Alonso")) {	
 			        dispose();
 					
 				}else{
@@ -159,13 +136,7 @@ public class Login extends JDialog {
 		});
 		button.setForeground(new Color(0, 0, 102));
 		button.setFont(new Font("Tahoma", Font.BOLD, 20));
-		button.setBounds(65, 473, 238, 48);
+		button.setBounds(265, 485, 271, 48);
 		panel_2.add(button);
-		
-		JLabel label_3 = new JLabel("Si desea Registrarse comunicese con soporte tecnico");
-		label_3.setForeground(new Color(0, 0, 102));
-		label_3.setFont(new Font("Tahoma", Font.BOLD, 11));
-		label_3.setBounds(23, 536, 315, 20);
-		panel_2.add(label_3);
 	}
 }
